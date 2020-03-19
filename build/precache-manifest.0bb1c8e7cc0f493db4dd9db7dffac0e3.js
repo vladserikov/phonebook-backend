c3bed4025baeda253f129833986562cfbc277ef4
Vlad Serikov <vladikserikov@gmail.com>
2020-03-19 19:40:44 +0700

    add build static

---
+++ build/precache-manifest.0bb1c8e7cc0f493db4dd9db7dffac0e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6164c0cf2036c5aa8c5305480532e1b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20072dfce21dede73afd",
+    "url": "/static/css/main.b43e38c3.chunk.css"
+  },
+  {
+    "revision": "af61a9ff62b070d108e1",
+    "url": "/static/js/2.2b991da7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.2b991da7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20072dfce21dede73afd",
+    "url": "/static/js/main.91988d1b.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);
--- index.js
+++ index.js
@@ -7,8 +7,9 @@ app.use(express.json())
 morgan.token('body', (req) => {
     return JSON.stringify(req.body)
 })
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+app.use(express.static('build'))
 
 let persons = [
     {
