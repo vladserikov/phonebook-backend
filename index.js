4e292a44fd21e7ea5cbd67735fce5401bbe1c5a2
Vlad Serikov <vladikserikov@gmail.com>
2020-03-19 15:28:06 +0700

    add delete request

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const persons = [
+let persons = [
     {
         "name": "Arto Hellas",
         "number": "040-123456",
@@ -63,7 +63,13 @@ app.get('/api/persons/:id', (req, res) => {
 
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    console.log(id)
 
+    persons = persons.filter(p => p.id !== id)
+    res.status(204).end()
+})
 
 const PORT = 3001;
 
